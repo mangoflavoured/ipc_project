043ad16d40c6783be97a5e1ca9446bb6bd4043eb
ledzep0830 <kevin830@naver.com>
2022-04-24 20:26:22 +0900

    22-1-os

---
+++ assignment/assignment_1_1/message_buffer.c
@@ -0,0 +1,87 @@
+#include "message_buffer.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int shmid;
+void *memory_segment=NULL;
+
+int init_buffer(MessageBuffer **buffer) {
+    /*---------------------------------------*/
+    /* TODO 1 : init buffer                  */
+
+    {}
+
+    /* TODO 1 : END                          */
+    /*---------------------------------------*/
+
+    printf("init buffer\n");
+    return 0;
+}
+
+int attach_buffer(MessageBuffer **buffer) {
+    /*---------------------------------------*/
+    /* TODO 2 : attach buffer                */
+    /* do not consider "no buffer situation" */
+    
+    {}
+
+    /* TODO 2 : END                          */
+    /*---------------------------------------*/
+
+    printf("attach buffer\n");
+    printf("\n");
+    return 0;
+}
+
+int detach_buffer() {
+    if (shmdt(memory_segment) == -1) {
+        printf("shmdt error!\n\n");
+        return -1;
+    }
+
+    printf("detach buffer\n\n");
+    return 0;
+}
+
+int destroy_buffer() {
+    if(shmctl(shmid, IPC_RMID, NULL) == -1) {
+        printf("shmctl error!\n\n");
+        return -1;
+    }
+
+    printf("destroy shared_memory\n\n");
+    return 0;
+}
+
+int produce(MessageBuffer **buffer, int sender_id, int data, int account_id) {
+
+    /*---------------------------------------*/
+    /* TODO 3 : produce message              */
+
+    {}
+
+    /* TODO 3 : END                          */
+    /*---------------------------------------*/
+
+    printf("produce message\n");
+
+    return 0;
+}
+
+int consume(MessageBuffer **buffer, Message **message) {
+    if((*buffer)->is_empty)
+	    return -1;
+
+    /*---------------------------------------*/
+    /* TODO 4 : consume message              */
+
+    {}
+
+    /* TODO 4 : END                          */
+    /*---------------------------------------*/
+
+    return 0;
+}
